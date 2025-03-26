8aa0a3564615e5905e788602a6d10d109a2b543d
Sami <samiisboss1574@gmail.com>
2025-03-26 15:17:21 +0530

    form 2 done

--- resources/views/form1.blade.php
+++ resources/views/form1.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,16 +10,20 @@
         .step {
             display: none;
         }
+
         .step.active {
             display: block;
         }
+
         .step3-container {
             display: flex;
             flex-direction: column;
-            gap: 15px; /* Added spacing */
+            gap: 15px;
+            /* Added spacing */
         }
     </style>
 </head>
+
 <body>
     <div class="container mt-5">
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#landFormModal">
@@ -36,84 +41,84 @@
                 <div class="modal-body">
                     <form id="landForm">
                         <div id="step1">
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="farmerName" class="form-label">Name of Farmer</label>
-                                <input type="text" class="form-control" id="farmerName" required>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="mobileNumber" class="form-label">Mobile Number</label>
-                                <input type="text" class="form-control" id="mobileNumber" required>
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label class="form-label">Gender</label>
-                                <div>
-                                    <input type="radio" id="male" name="gender" value="Male" class="form-check-input" required>
-                                    <label for="male" class="form-check-label">Male</label>
-                                    <input type="radio" id="female" name="gender" value="Female" class="form-check-input ms-3" required>
-                                    <label for="female" class="form-check-label">Female</label>
-                                    <input type="radio" id="transgender" name="gender" value="Transgender" class="form-check-input ms-3" required>
-                                    <label for="transgender" class="form-check-label">Transgender</label>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label for="farmerName" class="form-label">Name of Farmer</label>
+                                    <input type="text" class="form-control" id="farmerName" required>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="mobileNumber" class="form-label">Mobile Number</label>
+                                    <input type="text" class="form-control" id="mobileNumber" required>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <label for="fatherSpouse" class="form-label">Father / Spouse</label>
-                                <input type="text" class="form-control" id="fatherSpouse" required>
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="householdMembers" class="form-label">Household Members</label>
-                                <input type="number" class="form-control" id="householdMembers" required>
-                            </div>
-                            <div class="col-md-6">
-                                <label class="form-label">Identity Card</label>
-                                <div>
-                                    <input type="radio" id="aadhar" name="identityCard" value="Aadhar" class="form-check-input identity-option" required>
-                                    <label for="aadhar" class="form-check-label">Aadhar</label>
-                                    <input type="radio" id="epic" name="identityCard" value="EPIC" class="form-check-input ms-3 identity-option" required>
-                                    <label for="epic" class="form-check-label">EPIC</label>
-                                    <input type="radio" id="drivingLicense" name="identityCard" value="Driving License" class="form-check-input ms-3 identity-option" required>
-                                    <label for="drivingLicense" class="form-check-label">Driving License</label>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label class="form-label">Gender</label>
+                                    <div>
+                                        <input type="radio" id="male" name="gender" value="Male" class="form-check-input" required>
+                                        <label for="male" class="form-check-label">Male</label>
+                                        <input type="radio" id="female" name="gender" value="Female" class="form-check-input ms-3" required>
+                                        <label for="female" class="form-check-label">Female</label>
+                                        <input type="radio" id="transgender" name="gender" value="Transgender" class="form-check-input ms-3" required>
+                                        <label for="transgender" class="form-check-label">Transgender</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="fatherSpouse" class="form-label">Father / Spouse</label>
+                                    <input type="text" class="form-control" id="fatherSpouse" required>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="idCardNumber" class="form-label">ID Card Number</label>
-                                <input type="text" class="form-control" id="idCardNumber" required>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label for="householdMembers" class="form-label">Household Members</label>
+                                    <input type="number" class="form-control" id="householdMembers" required>
+                                </div>
+                                <div class="col-md-6">
+                                    <label class="form-label">Identity Card</label>
+                                    <div>
+                                        <input type="radio" id="aadhar" name="identityCard" value="Aadhar" class="form-check-input identity-option" required>
+                                        <label for="aadhar" class="form-check-label">Aadhar</label>
+                                        <input type="radio" id="epic" name="identityCard" value="EPIC" class="form-check-input ms-3 identity-option" required>
+                                        <label for="epic" class="form-check-label">EPIC</label>
+                                        <input type="radio" id="drivingLicense" name="identityCard" value="Driving License" class="form-check-input ms-3 identity-option" required>
+                                        <label for="drivingLicense" class="form-check-label">Driving License</label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <label for="fileUpload" class="form-label" id="fileUploadLabel">Upload ID Proof</label>
-                                <input type="file" class="form-control" id="fileUpload" required>
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="hamlet" class="form-label">Hamlet</label>
-                                <input type="text" class="form-control" id="hamlet" required>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label for="idCardNumber" class="form-label">ID Card Number</label>
+                                    <input type="text" class="form-control" id="idCardNumber" required>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="fileUpload" class="form-label" id="fileUploadLabel">Upload ID Proof</label>
+                                    <input type="file" class="form-control" id="fileUpload" required>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <label for="panchayat" class="form-label">Panchayat</label>
-                                <input type="text" class="form-control" id="panchayat" required>
+
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label for="hamlet" class="form-label">Hamlet</label>
+                                    <input type="text" class="form-control" id="hamlet" required>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="panchayat" class="form-label">Panchayat</label>
+                                    <input type="text" class="form-control" id="panchayat" required>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="block" class="form-label">Block</label>
-                                <input type="text" class="form-control" id="block" required>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label for="block" class="form-label">Block</label>
+                                    <input type="text" class="form-control" id="block" required>
+                                </div>
                             </div>
-                        </div>
 
 
-                        <button type="button" class="btn btn-primary" onclick="nextStep(1, 2)">Next</button>
+                            <button type="button" class="btn btn-primary" onclick="nextStep(1, 2)">Next</button>
                         </div>
 
                         <div id="step2" style="display: none;">
@@ -146,9 +151,9 @@
                             </div>
                             <button type="button" class="btn btn-secondary" onclick="prevStep(2, 1)">Back</button>
                             <button type="button" class="btn btn-primary" onclick="nextStep(2, 3)">Next</button>
-</div>
+                        </div>
 
-                            <div id="step3" style="display: none;">
+                        <div id="step3" style="display: none;">
                             <h5>Land Development Details</h5>
                             <div class="row mb-3">
                                 <div class="col-md-6">
@@ -206,17 +211,18 @@
             document.getElementById('step' + current).style.display = 'none';
             document.getElementById('step' + next).style.display = 'block';
         }
-        
+
         function prevStep(current, prev) {
             document.getElementById('step' + current).style.display = 'none';
             document.getElementById('step' + prev).style.display = 'block';
         }
 
         function updateTitle() {
-        const selectedIdentity = document.querySelector('input[name="identity"]:checked').value;
-        document.getElementById('step1Title').innerText = `You Selected: ${selectedIdentity}`;
-    }
+            const selectedIdentity = document.querySelector('input[name="identity"]:checked').value;
+            document.getElementById('step1Title').innerText = `You Selected: ${selectedIdentity}`;
+        }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
