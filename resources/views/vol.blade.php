98f75b0d678aabd5ff94a91f1085919d220a116d
Saran0605 <saransoundharajan05@gmail.com>
2025-04-05 21:26:01 +0530

    dashboard

--- resources/views/vol.blade.php
+++ resources/views/vol.blade.php
@@ -180,17 +180,10 @@
                             <div class="card tale-bg p-3">
                                 <div class="row"> <!-- Use row to align cards side by side -->
 
-                                    <!-- User Photo Card -->
-                                    <div class="col-md-6">
-                                        <div class="card shadow card-tale text-center">
-                                            <div class="card-body">
-                                                <img src="assets/images/faces/face28.jpg" alt="User Photo" class="img-fluid rounded-circle mt-1" style="width: 175px; height: 175px; object-fit: cover;">
-                                            </div>
-                                        </div>
-                                    </div>
+                                 
                                     
                                     <!-- User Info Card -->
-                                    <div class="col-md-6">
+                                    <div class="col-md-12">
                                         <div class="card shadow card-light-danger">
                                             <div class="card-body">
                                                 <h5 class="card-title text-dark">User Details</h5>
@@ -251,120 +244,7 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-12 grid-margin stretch-card">
-                            <div class="card position-relative">
-                                <div class="card-body">
-                                    <div id="detailedReports" class="carousel slide detailed-report-carousel position-static pt-2"
-                                        data-bs-ride="carousel">
-                                        <div class="carousel-inner">
-                                            <div class="carousel-item active">
-                                                <div class="row">
-                                                    <div class="col-md-12 col-xl-3 d-flex flex-column justify-content-start">
-                                                        <div class="ml-xl-4 mt-3">
-                                                            <p class="card-title">Detailed Reports</p>
-                                                            <h1 class="text-primary">&#8377;<b>0</b></h1>
-                                                            <h3 class="font-weight-500 mb-xl-4 text-primary">India</h3>
-                                                            <p class="mb-2 mb-xl-0">The Amount Generated in Farm Lands and Ponds all over India.</p>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-12 col-xl-9">
-                                                        <div class="row">
-                                                            <div class="col-md-6 border-right">
-                                                                <div class="table-responsive mb-3 mb-md-0 mt-3">
-                                                                    <table class="table table-borderless report-table">
-                                                                        <tr>
-                                                                            <td class="text-muted">Tamil Nadu</td>
-                                                                            <td class="w-100 px-0">
-                                                                                <div class="progress progress-md mx-4">
-                                                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 70%"
-                                                                                        aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                                </div>
-                                                                            </td>
-                                                                            <td>
-                                                                                <h5 class="font-weight-bold mb-0">713</h5>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td class="text-muted">kerala</td>
-                                                                            <td class="w-100 px-0">
-                                                                                <div class="progress progress-md mx-4">
-                                                                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 30%"
-                                                                                        aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                                </div>
-                                                                            </td>
-                                                                            <td>
-                                                                                <h5 class="font-weight-bold mb-0">583</h5>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td class="text-muted">Meghalaya</td>
-                                                                            <td class="w-100 px-0">
-                                                                                <div class="progress progress-md mx-4">
-                                                                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 95%"
-                                                                                        aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                                </div>
-                                                                            </td>
-                                                                            <td>
-                                                                                <h5 class="font-weight-bold mb-0">924</h5>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td class="text-muted">Karnataka</td>
-                                                                            <td class="w-100 px-0">
-                                                                                <div class="progress progress-md mx-4">
-                                                                                    <div class="progress-bar bg-info" role="progressbar" style="width: 60%"
-                                                                                        aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                                </div>
-                                                                            </td>
-                                                                            <td>
-                                                                                <h5 class="font-weight-bold mb-0">664</h5>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td class="text-muted">Andra Pradesh</td>
-                                                                            <td class="w-100 px-0">
-                                                                                <div class="progress progress-md mx-4">
-                                                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 40%"
-                                                                                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                                </div>
-                                                                            </td>
-                                                                            <td>
-                                                                                <h5 class="font-weight-bold mb-0">560</h5>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td class="text-muted">Telangana</td>
-                                                                            <td class="w-100 px-0">
-                                                                                <div class="progress progress-md mx-4">
-                                                                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 75%"
-                                                                                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                                </div>
-                                                                            </td>
-                                                                            <td>
-                                                                                <h5 class="font-weight-bold mb-0">793</h5>
-                                                                            </td>
-                                                                        </tr>
-                                                                    </table>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-6 mt-3">
-                                                                <div class="daoughnutchart-wrapper">
-                                                                    <canvas id="north-america-chart"></canvas>
-                                                                </div>
-                                                                <div id="north-america-chart-legend">
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
                 <footer class="footer">
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
