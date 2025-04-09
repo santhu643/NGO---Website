4febc912062bbc19244dd1ca3cd5a3a3ddd2cc66
Saran0605 <saransoundharajan05@gmail.com>
2025-04-09 16:05:10 +0530

    form modifications

--- resources/views/form1.blade.php
+++ resources/views/form1.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
 
+    
     <link rel="stylesheet" href="{{ asset('assets/vendors/ti-icons/css/themify-icons.css') }}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
